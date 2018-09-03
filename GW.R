9f21253d6f979278e7307ee67028e3ffe80078f4
Owais Raza <reye27@gmail.com>
2018-09-03 11:55:50 +0430

    Update GW.R
    extracting most influential variables in various GWPCs

--- GW.R
+++ GW.R
@@ -366,3 +366,226 @@ m %>% addPolygons(
             pal = pal, 
             values = ea.border$ea.ptv6.k8)
 
+# extracting most influential variables in GWPC1
+loadings.pc1.k8=gwpca.k8$loadings[,,1] # View(loadings.pc1.k8)
+PC1.WinVar.k8=max.col(abs(loadings.pc1.k8)) # summary(PC1.WinVar.k8) ?Is it a position of the kth variable? 
+ea.border$PC1.WinVar.k8=PC1.WinVar.k8
+table(PC1.WinVar.k8)
+
+# mapping
+ea.border$PC1.WinVar.k8.cat <- as.factor(ea.border$PC1.WinVar.k8)
+levels(ea.border$PC1.WinVar.k8.cat) <-c("Poor", "Rural", "Solid Fuel",
+                                        "Healthcare Access",
+                                        "Mother Edu < Secondary",
+                                        "Mother Edu >= Secondary", 
+                                        "Father Edu < Secondary", 
+                                        "Father Edu >= Secondary",
+                                        "Mother Occ - Prof",
+                                        "Unimproved Drinking Water",
+                                        "Water at Home")
+
+factpal <- colorFactor(mypal.11, ea.border$PC1.WinVar.k8.cat)
+
+m %>% 
+  addPolygons(
+    stroke = FALSE, smoothFactor = 0.2, fillOpacity = 0.5,
+    color = ~factpal(ea.border$PC1.WinVar.k8.cat)
+  ) %>%
+  addLegend("bottomright", 
+            title = "Variables with max loadings <br> within 1st component",
+            pal = factpal, 
+            values = ea.border$PC1.WinVar.k8.cat)
+
+# extracting most influential variables in GWPC2
+loadings.pc2.k8=gwpca.k8$loadings[,,2]
+PC2.WinVar.k8=max.col(abs(loadings.pc2.k8))
+ea.border$PC2.WinVar.k8=PC2.WinVar.k8
+table(PC2.WinVar.k8)
+
+# mapping
+ea.border$PC2.WinVar.k8.cat <- as.factor(ea.border$PC2.WinVar.k8)
+levels(ea.border$PC2.WinVar.k8.cat) <-c("Poor", "Rural", "Solid Fuel",
+                                        "Single Mother",
+                                        "Mother Occ - None",
+                                        "Girl Child",
+                                        "Unimproved Drinking Water", 
+                                        "Complete Immunization", "Aridity",
+                                        "Max Temperature","PM2.5","Drought Hazard",
+                                        "6-11 months", "12-23 months",
+                                        "24-35 months", "36-47 months",
+                                        "48-59 months")
+
+factpal <- colorFactor(mypal.17, ea.border$PC2.WinVar.k8.cat)
+m %>% 
+  addPolygons(
+    stroke = FALSE, smoothFactor = 0.2, fillOpacity = 0.7,
+    color = ~factpal(ea.border$PC2.WinVar.k8.cat)
+  ) %>%
+  addLegend("bottomright", 
+            title = "Variables with max loadings <br> within 2nd component",
+            pal = factpal, 
+            values = ea.border$PC2.WinVar.k8.cat)
+
+# extracting most influential variables in GWPC3
+loadings.pc3.k8=gwpca.k8$loadings[,,3]
+PC3.WinVar.k8=max.col(abs(loadings.pc3.k8))
+ea.border$PC3.WinVar.k8=PC3.WinVar.k8
+table(PC3.WinVar.k8)
+
+# mapping
+ea.border$PC3.WinVar.k8.cat <- as.factor(ea.border$PC3.WinVar.k8)
+levels(ea.border$PC3.WinVar.k8.cat) <-c("Solid Fuel","Father Edu < Secondary",
+                                        "Mother Occ - None", "Girl Child",
+                                        "Complete Immunization", 
+                                        "Maximum Temperature", "PM2.5",
+                                        "< 6 months",
+                                        "6-11 months",
+                                        "12-23 months",
+                                        "24-35 months",
+                                        "36-47 months",
+                                        "48-59 months")
+
+factpal <- colorFactor(mypal.17, ea.border$PC3.WinVar.k8.cat)
+
+m %>% 
+  addPolygons(
+    stroke = FALSE, smoothFactor = 0.2, fillOpacity = 0.7,
+    color = ~factpal(ea.border$PC3.WinVar.k8.cat)
+  ) %>%
+  addLegend("bottomright", 
+            title = "Variables with max loadings <br> within 3rd component",
+            pal = factpal, 
+            values = ea.border$PC3.WinVar.k8.cat)
+
+# extracting most influential variables in GWPC4
+loadings.pc4.k8=gwpca.k8$loadings[,,4]
+PC4.WinVar.k8=max.col(abs(loadings.pc4.k8))
+ea.border$PC4.WinVar.k8=PC4.WinVar.k8
+table(PC4.WinVar.k8)
+
+# mapping
+ea.border$PC4.WinVar.k8.cat <- as.factor(ea.border$PC4.WinVar.k8)
+levels(ea.border$PC4.WinVar.k8.cat) <-c("Solid Fuel","Healthcare Access",
+                                        "Single Mother",
+                                        "Mother Occ - None",
+                                        "Mother Occ - Prof",
+                                        "Girl Child",
+                                        "Unimproved Drinking Water",
+                                        "Complete Immunization", 
+                                        "Maximum Temperature",
+                                        "< 6 months",
+                                        "6-11 months",
+                                        "12-23 months",
+                                        "24-35 months",
+                                        "36-47 months",
+                                        "48-59 months")
+
+factpal <- colorFactor(mypal.15, ea.border$PC4.WinVar.k8.cat)
+
+m %>% 
+  addPolygons(
+    stroke = FALSE, smoothFactor = 0.2, fillOpacity = 0.7,
+    color = ~factpal(ea.border$PC4.WinVar.k8.cat)
+  ) %>%
+  addLegend("bottomright", 
+            title = "Variables with max loadings <br> within 4th component",
+            pal = factpal, 
+            values = ea.border$PC4.WinVar.k8.cat)
+
+# extracting most influential variables in GWPC5
+loadings.pc5.k8=gwpca.k8$loadings[,,5]
+PC5.WinVar.k8=max.col(abs(loadings.pc5.k8))
+ea.border$PC5.WinVar.k8=PC5.WinVar.k8
+table(PC5.WinVar.k8)
+
+# mapping
+ea.border$PC5.WinVar.k8.cat <- as.factor(ea.border$PC5.WinVar.k8)
+levels(ea.border$PC5.WinVar.k8.cat) <-c("Poor","Solid Fuel", 
+                                        "Healthcare Access",
+                                        "Single Mother",
+                                        "Mother Edu < Secondary",
+                                        "Mother Occ - None",
+                                        "Mother Occ - Prof",
+                                        "Girl Child",
+                                        "Unimproved Drinking Water",
+                                        "Complete Immunization",
+                                        "Maximum Temperature",
+                                        "< 6 months",
+                                        "6-11 months",
+                                        "12-23 months",
+                                        "24-35 months",
+                                        "36-47 months",
+                                        "48-59 months")
+
+factpal <- colorFactor(mypal.17, ea.border$PC5.WinVar.k8.cat)
+
+m %>% 
+  addPolygons(
+    stroke = FALSE, smoothFactor = 0.2, fillOpacity = 0.7,
+    color = ~factpal(ea.border$PC5.WinVar.k8.cat)
+  ) %>%
+  addLegend("bottomright", 
+            title = "Variables with max loadings <br> within 5th component",
+            pal = factpal, 
+            values = ea.border$PC5.WinVar.k8.cat)
+
+# extracting most influential variables in GWPC6
+loadings.pc6.k8=gwpca.k8$loadings[,,6]
+PC6.WinVar.k8=max.col(abs(loadings.pc6.k8))
+ea.border$PC6.WinVar.k8=PC6.WinVar.k8
+table(PC6.WinVar.k8)
+
+# mapping
+ea.border$PC6.WinVar.k8.cat <- as.factor(ea.border$PC6.WinVar.k8)
+levels(ea.border$PC6.WinVar.k8.cat) <-c("Solid Fuel", 
+                                        "Healthcare Access",
+                                        "Single Mother",
+                                        "Mother Occ - None",
+                                        "Mother Occ - Prof",
+                                        "Girl Child",
+                                        "Unimproved Drinking Water",
+                                        "Complete Immunization",
+                                        "Maximum Temperature",
+                                        "< 6 months",
+                                        "6-11 months",
+                                        "12-23 months",
+                                        "24-35 months",
+                                        "36-47 months",
+                                        "48-59 months")
+
+factpal <- colorFactor(mypal.17, ea.border$PC6.WinVar.k8.cat)
+
+m %>% 
+  addPolygons(
+    stroke = FALSE, smoothFactor = 0.2, fillOpacity = 0.7,
+    color = ~factpal(ea.border$PC6.WinVar.k8.cat)
+  ) %>%
+  addLegend("bottomright", 
+            title = "Variables with max loadings <br> within 6th component",
+            pal = factpal, 
+            values = ea.border$PC6.WinVar.k8.cat)
+
+## Monte Carlo Test for first component (prespecified BW)
+## Simulation (n=1000) with Monte Carlo Test for 
+## first component (auto-calibirated BW )
+## estimated time ~ 15 hours
+
+gwpca.mc27.1000 <- montecarlo.gwpca.2(ea.scaled.spdf, 
+                                      vars = colnames(ea.scaled.spdf@data[,1:27]),
+                                      k = 27, nsims=1000,
+                                      adaptive = TRUE)
+with(ea.scaled.spdf@data[,1:27],{
+  plot(density(gwpca.mc10.1000$sims),
+       main = "Test statistics for eigenvalue nonstationarity",
+       xlab = "SD of local eigenvalues from randomisations \n N = 1000")
+  abline(v=2.160, col = "red", lwd = 2)
+  text(2.25, 2.5, paste("Observed SD of local eigenvalues \n p value = 0.0160"), col = "red",
+       srt = 90)
+  abline(v = 0.8164263, col = "darkblue", lwd = 2, lty = 5)
+  text(0.9, 2.5, paste("Mean of SD of local \n eigenvalues from randomisations"),
+       col = "darkblue", srt = 90)
+  rug(gwpca.mc10.1000$sims, col = "orange")
+})
+abline(h=-0.1, v=2.15900, col = "red", lwd = 2)
+
+
